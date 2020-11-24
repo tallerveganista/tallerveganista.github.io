712b296e05a6c9a832087ea136596a617c14667f
chicoesqueleto <chicoesqueleto@gmail.com>
2020-11-24 15:47:31 -0500

    animation on shrink nav slide top

--- js/nav-shrink.js
+++ js/nav-shrink.js
@@ -1,7 +1,7 @@
 $(window).scroll(function() {
-  if ($(document).scrollTop() > 32) {
-    $('.main-nav').addClass('shrink-nav');
+  if ($(document).scrollTop() > 48) {
+    $('.main-nav').addClass('shrink-nav uk-animation-slide-top');
   } else {
-    $('.main-nav').removeClass('shrink-nav');
+    $('.main-nav').removeClass('shrink-nav uk-animation-slide-top');
   }
 });
