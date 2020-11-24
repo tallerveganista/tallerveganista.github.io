edf160d30669af29912afc48ae7b3e9191959166
chicoesqueleto <chicoesqueleto@gmail.com>
2020-11-24 15:25:15 -0500

    main nav no animation

--- js/nav-shrink.js
+++ js/nav-shrink.js
@@ -1,5 +1,5 @@
 $(window).scroll(function() {
-  if ($(document).scrollTop() > 50) {
+  if ($(document).scrollTop() > 32) {
     $('.main-nav').addClass('shrink-nav');
   } else {
     $('.main-nav').removeClass('shrink-nav');
