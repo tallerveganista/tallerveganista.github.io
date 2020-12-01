b3b385cc25cc2662d1f0d1af12820405e4e866ac
chicoesqueleto <chicoesqueleto@gmail.com>
2020-11-30 23:48:47 -0500

    alert

--- assets/js/nav-shrink.js
+++ assets/js/nav-shrink.js
@@ -1,5 +1,5 @@
 $(window).scroll(function() {
-  if ($(document).scrollTop() > 10) {
+  if ($(document).scrollTop() > 4) {
     $('#desk-nav').attr("uk-sticky", "bottom: #offset");
   } else {
     $('#desk-nav').removeAttr("uk-sticky", "bottom: #offset");
@@ -7,7 +7,7 @@ $(window).scroll(function() {
 });
 
 $(window).scroll(function() {
-  if ($(document).scrollTop() > 48) {
+  if ($(document).scrollTop() > 38) {
     $('#desk-nav').addClass('shrink-nav uk-animation-slide-top');
   } else {
     $('#desk-nav').removeClass('shrink-nav uk-animation-slide-top');
